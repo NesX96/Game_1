1fac70d1ac0cb70e68955a313bd9019f42620b50
nesis <addis.paoloe@hotmail.it>
2018-03-01 18:16:49 +0100

    Main adjustments

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Creazione del main");
+        System.out.println("Welcome to this Game!");
     }
 
 }
